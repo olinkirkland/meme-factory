a6185fca1782404b16758a0273697ea60d7c9035
Olin Kirkland <me@olinkirk.land>
2022-03-18 10:36:02 +0100

    Initial struct

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      <section>
+        <p>© 2021 by The Meme Factory</p>
+      </section>
+    </footer>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header>
+      <section>
+        <h1>
+          The <span class="logo-badge">Meme</span> Factory
+        </h1>
+      </section>
+    </header>
+  );
+}
